72121d28b29d452076f1921d7ff78ae0781c8618
addygeek <ui22cs03@iiitsurat.ac.in>
2024-07-04 02:23:05 +0530

    tnp-officer Dr. Vijaykumar Radadiya page done

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,21 @@ import './index.css'
 import Navbarr from './components/Navbarr';
 import Home from './Pages/Home';
 import AboutTnp from './components/AboutTnp';
+import AboutTnp_Tnp_Officer from './components/AboutTnp_Tnp_Officer';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <div className={`${darkMode && 'dark'}`}>
+   
+        <div className={`${darkMode && 'dark'}`}>
       <Router>
         <Navbarr setDarkMode={setDarkMode} darkMode={darkMode} />
         <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/vision' element={<Eleven />} />
             <Route path='/about-tnp' element={<AboutTnp/>} />
+            <Route path='/tnp-officer' element={<AboutTnp_Tnp_Officer/>} />
         </Routes>
         <Footer />
       </Router>
