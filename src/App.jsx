cd5b57e72e0a78368ee93ea7a2f3dab60a348385
PrateekJaiswal16 <prateekja.swal16@gmail.com>
2024-07-06 22:16:02 +0530

    Adding Director Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Eleven  from './Pages/11';
+import Nine from './Pages/9';
 import Footer from './components/footer';
 import './index.css'
 import Navbarr from './components/Navbarr';
@@ -21,6 +22,8 @@ function App() {
             <Route path='/department' element={<Departments />} />
             <Route path='/vision' element={<Eleven />} />
             <Route path='/about-tnp' element={<AboutTnp/>} />
+            <Route path='/about-tnp' element={<AboutTnp/>} />
+            <Route path='/director' element={<Nine/>} />
         </Routes>
         <Footer />
       </Router>
