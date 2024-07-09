db503b2997cc313b7288ebf67193d6680c84d2ce
1234Priyanshu4321 <chaturvedipriyanshu17@gmail.com>
2024-07-09 13:48:39 +0530

    Department page updates2

--- src/containers/03_temp.jsx
+++ src/containers/03_temp.jsx
@@ -1,14 +1,11 @@
-
-
-
 const ThreeComponent = () => {
     return (
-        <div className="flex flex-col ml-5 w-[83%] max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col mt-2.5 text-xl font-semibold text-red-950 max-md:mt-10 max-md:max-w-full">
+        <div className="flex flex-col ml-5 w-[83%] max-md:ml-0 max-md:w-full my-6">
+            <div className="flex flex-col mt-2.5 text-xl  max-md:mt-10 max-md:max-w-full">
                 <div className="flex gap-5 max-md:flex-wrap">
-                    <div className="grow">About department</div>
+                    <div className="grow font-semibold text-red-800">About department</div>
                 </div>
-                <div className="mt-9 text-lg text-justify text-zinc-700 max-md:max-w-full">
+                <div className="mt-9 text-sm text-justify max-md:max-w-full">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
                     placeat fugiat necessitatibus. Et ab id minus tenetur consectetur
                     molestias expedita minima delectus hic facere dicta rem fugit
@@ -35,8 +32,8 @@ const ThreeComponent = () => {
                     quis provident omnis nemo placeat quo rerum iste sed sequi eaque
                     animi quidem commodi voluptas repellat obcaecati quos sint
                 </div>
-                <div className="mt-8">Dr. Pradeep Kumar Roy</div>
-                <div className="mt-3.5 text-base font-medium text-neutral-500">
+                <div className="mt-8 text-base text-red-800 font-semibold">Dr. Pradeep Kumar Roy</div>
+                <div className="mt-3.5 text-base font-medium text-base text-neutral-500">
                     Head of Department
                 </div>
             </div>
@@ -45,4 +42,4 @@ const ThreeComponent = () => {
     )
 }
 
-export default ThreeComponent;
+export default ThreeComponent;
