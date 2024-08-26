a06b94d2cc1ef6e8e4e1cf0a86ef42af108c3c90
devikaverma155 <devikaverma1554@gmail.com>
2024-08-26 09:45:22 +0530

    15 ,16

--- IIIT-Surat-Web-Frontend-main/src/components/Tnpsection/15.jsx
+++ IIIT-Surat-Web-Frontend-main/src/components/Tnpsection/15.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import image1 from '../../assets/images/1TNP.png';
+import image2 from '../../assets/images/2TNP.png';
+import image3 from '../../assets/images/3TNP.png';
+import image4 from '../../assets/images/4TNP.png';
+import image5 from '../../assets/images/5TNP.png';
+import image6 from '../../assets/images/6TNP.png';
 
 const Tnp15 = () => {
   return (
-    <div>Tnp13</div>
-  )
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-6 text-blue-900 ">Placement Statistics</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 mx-auto max-w-4xl md:ml--2">
+        {[image1, image2, image3, image4, image5, image6].map((image, index) => (
+          <div key={index} className="w-full aspect-square">
+            <img 
+              src={image} 
+              alt={`Placement statistic ${index + 1}`} 
+              className="w-full h-full object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Tnp15
+export default Tnp15;
--- IIIT-Surat-Web-Frontend-main/src/components/Tnpsection/16.jsx
+++ IIIT-Surat-Web-Frontend-main/src/components/Tnpsection/16.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import image1 from "../../assets/images/Achievers/1.png";
+import image2 from "../../assets/images/Achievers/2.png";
+import image3 from "../../assets/images/Achievers/3.png";
+import image4 from "../../assets/images/Achievers/4.png";
+import image5 from "../../assets/images/Achievers/5.png";
+import image6 from "../../assets/images/Achievers/6.png";
+import image7 from "../../assets/images/Achievers/7.png";
+import image8 from "../../assets/images/Achievers/8.png";
+import image9 from "../../assets/images/Achievers/9.png";
+import image10 from "../../assets/images/Achievers/10.png";
+import image11 from "../../assets/images/Achievers/11.png";
+import image12 from "../../assets/images/Achievers/12.png";
+import image13 from "../../assets/images/Achievers/13.png";
+import image14 from "../../assets/images/Achievers/14.png";
+import image15 from "../../assets/images/Achievers/15.png";
+import image16 from "../../assets/images/Achievers/16.png";
 
 const Tnp16 = () => {
-  return (
-    <div>16</div>
-  )
-}
+  const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16];
 
-export default Tnp16
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-6 text-blue-900">Our Achievers</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-6xl mx-auto">
+        {images.map((image, index) => (
+          <div key={index} className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg">
+            <img 
+              src={image} 
+              alt={`Achiever ${index + 1}`} 
+              className="w-full h-full object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Tnp16;
