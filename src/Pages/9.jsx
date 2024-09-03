3cf0785f2788c600e0db45c1d0b16fb60ee67287
PrateekJaiswal16 <prateekja.swal16@gmail.com>
2024-09-03 13:10:19 +0530

    Director Desk Update

--- src/Pages/9.jsx
+++ src/Pages/9.jsx
@@ -8,17 +8,29 @@ export default function Nine() {
       <main className=" flex mt-16 mx-36">
         <DirectorImg />
         <DirectorInfo />
-
       </main>
-      <section className=" flex mt-16 mx-36">
-        <div className=" min-w-[50rem] mr-8">
-          <img className="rounded-xl" src={video} alt="vid" />
-        </div>
-        <div>
-            <h1 className="text-xl text-[#932323] text-justify font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, debitis!</h1>
-            <Info>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo corrupti aliquid ipsam quod labore omnis rerum ab modi perferendis optio dignissimos veritatis eos asperiores eligendi, in quas veniam molestias. Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, porro?</Info>
+      <section className=" flex my-16 mx-24">
+        <div className=" min-w-[50rem] mr-8 ">
+          <img className="rounded-xl float-left max-w-[40rem] mr-6 mb-6" src={video} alt="vid" />
+          <div>
+              <h1 className="text-xl text-[#932323] text-justify font-bold">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate, debitis!
+              </h1>
+            <Info>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis illo corrupti aliquid ipsam quod labore omnis rerum
+              ab modi perferendis optio dignissimos veritatis eos asperiores
+              eligendi, in quas veniam molestias. Lorem ipsum dolor sit amet
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quam, porro?
+            </Info>
           </div>
+        </div>
+        {/* <div>
+            
+          </div> */}
       </section>
     </>
-  )
+  );
 }
